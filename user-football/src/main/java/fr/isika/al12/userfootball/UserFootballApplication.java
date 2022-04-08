a4230b3fbf2e481e6737c170e8f81ad9125a2462
AlexisAbrate <abrate.alexis@outlook.fr>
2022-04-08 17:16:03 +0200

    premier commit

---
+++ user-football/src/main/java/fr/isika/al12/userfootball/UserFootballApplication.java
@@ -0,0 +1,13 @@
+package fr.isika.al12.userfootball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserFootballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserFootballApplication.class, args);
+	}
+
+}
---
+++ user-football/src/test/java/fr/isika/al12/userfootball/UserFootballApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.isika.al12.userfootball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserFootballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
